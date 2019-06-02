26ad2716398b19006ccb3f5462e54c6d246110c2
Mateusz Cegiełka <mateusz@cegla.net>
2019-06-02 10:16:04 +0200

    fix disappearing test view on update

--- src/test/view/manage.rs
+++ src/test/view/manage.rs
@@ -3,11 +3,13 @@ use crate::{
 		self, view::{render::render, SCROLL_TO_FIRST_FAILED}, TestRun
 	}, util
 };
-use evscode::{goodies::WebviewHandle, Webview, WebviewResultmap, E, R};
+use evscode::{
+	goodies::{webview_resultmap::Computation, WebviewHandle}, Webview, WebviewResultmap, E, R
+};
 use std::{fs, path::PathBuf};
 
 lazy_static::lazy_static! {
-	pub static ref COLLECTION: WebviewResultmap<Option<PathBuf>, Report> = WebviewResultmap::new(compute, create, manage);
+	pub static ref COLLECTION: WebviewResultmap<TestViewLogic> = WebviewResultmap::new(TestViewLogic);
 }
 
 pub fn touch_input(webview: &Webview) {
@@ -16,66 +18,77 @@ pub fn touch_input(webview: &Webview) {
 	});
 }
 
-fn compute(source: &Option<PathBuf>) -> R<Report> {
-	Ok(Report { runs: test::run(source)? })
-}
+pub struct TestViewLogic;
 
-fn create(source: &Option<PathBuf>, report: &Report) -> R<Webview> {
-	let title = util::fmt_verb("ICIE Test View", &source);
-	let webview = evscode::Webview::new("icie.test.view", title, 2).enable_scripts().retain_context_when_hidden().create();
-	webview.set_html(render(&report.runs)?);
-	webview.reveal(2);
-	if *SCROLL_TO_FIRST_FAILED.get() {
-		webview.post_message(json::object! {
-			"tag" => "scroll_to_wa",
-		});
+impl Computation for TestViewLogic {
+	type K = Option<PathBuf>;
+	type V = Report;
+
+	fn compute(&self, source: &Option<PathBuf>) -> R<Report> {
+		Ok(Report { runs: test::run(source)? })
 	}
-	Ok(webview)
-}
 
-fn manage(source: &Option<PathBuf>, _: &Report, webview: WebviewHandle) -> R<Box<dyn FnOnce()+Send+'static>> {
-	let webview = webview.lock().unwrap();
-	let stream = webview.listener().spawn().cancel_on(webview.disposer());
-	let source = source.clone();
-	Ok(Box::new(move || {
-		for note in stream {
-			match note["tag"].as_str() {
-				Some("trigger_rr") => evscode::spawn({
-					let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
-					let source = source.clone();
-					move || crate::debug::rr(in_path, source)
-				}),
-				Some("trigger_gdb") => evscode::spawn({
-					let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
-					let source = source.clone();
-					move || crate::debug::gdb(in_path, source)
-				}),
-				Some("new_test") => evscode::spawn(move || crate::test::add(note["input"].as_str().unwrap(), note["desired"].as_str().unwrap())),
-				Some("set_alt") => evscode::spawn({
-					let source = source.clone();
-					move || {
-						let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
-						let out = note["out"].as_str().unwrap();
-						fs::write(in_path.with_extension("alt.out"), format!("{}\n", out.trim()))
-							.map_err(|e| E::from_std(e).context("failed to save alternative out as a file"))?;
-						COLLECTION.get_force(source)?;
-						Ok(())
-					}
-				}),
-				Some("del_alt") => evscode::spawn({
-					let source = source.clone();
-					move || {
-						let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
-						fs::remove_file(in_path.with_extension("alt.out")).map_err(|e| E::from_std(e).context("failed to remove alternative out file"))?;
-						COLLECTION.get_force(source)?;
-						Ok(())
-					}
-				}),
-				Some("edit") => evscode::open_editor(note["path"].as_str().unwrap(), None, None),
-				_ => log::error!("unrecognied testview webview food `{}`", note.dump()),
-			}
+	fn create_empty_webview(&self, source: &Option<PathBuf>) -> R<Webview> {
+		let title = util::fmt_verb("ICIE Test View", &source);
+		let webview = evscode::Webview::new("icie.test.view", title, 2).enable_scripts().retain_context_when_hidden().create();
+		Ok(webview)
+	}
+
+	fn update(&self, _: &Option<PathBuf>, report: &Report, webview: &Webview) -> R<()> {
+		webview.set_html(render(&report.runs)?);
+		webview.reveal(2);
+		if *SCROLL_TO_FIRST_FAILED.get() {
+			webview.post_message(json::object! {
+				"tag" => "scroll_to_wa",
+			});
 		}
-	}))
+		Ok(())
+	}
+
+	fn manage(&self, source: &Option<PathBuf>, _: &Report, webview: WebviewHandle) -> R<Box<dyn FnOnce()+Send+'static>> {
+		let webview = webview.lock().unwrap();
+		let stream = webview.listener().spawn().cancel_on(webview.disposer());
+		let source = source.clone();
+		Ok(Box::new(move || {
+			for note in stream {
+				match note["tag"].as_str() {
+					Some("trigger_rr") => evscode::spawn({
+						let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
+						let source = source.clone();
+						move || crate::debug::rr(in_path, source)
+					}),
+					Some("trigger_gdb") => evscode::spawn({
+						let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
+						let source = source.clone();
+						move || crate::debug::gdb(in_path, source)
+					}),
+					Some("new_test") => evscode::spawn(move || crate::test::add(note["input"].as_str().unwrap(), note["desired"].as_str().unwrap())),
+					Some("set_alt") => evscode::spawn({
+						let source = source.clone();
+						move || {
+							let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
+							let out = note["out"].as_str().unwrap();
+							fs::write(in_path.with_extension("alt.out"), format!("{}\n", out.trim()))
+								.map_err(|e| E::from_std(e).context("failed to save alternative out as a file"))?;
+							COLLECTION.get_force(source)?;
+							Ok(())
+						}
+					}),
+					Some("del_alt") => evscode::spawn({
+						let source = source.clone();
+						move || {
+							let in_path = PathBuf::from(note["in_path"].as_str().unwrap());
+							fs::remove_file(in_path.with_extension("alt.out")).map_err(|e| E::from_std(e).context("failed to remove alternative out file"))?;
+							COLLECTION.get_force(source)?;
+							Ok(())
+						}
+					}),
+					Some("edit") => evscode::open_editor(note["path"].as_str().unwrap(), None, None),
+					_ => log::error!("unrecognied testview webview food `{}`", note.dump()),
+				}
+			}
+		}))
+	}
 }
 
 pub struct Report {
