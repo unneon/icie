f9f7a8c08f8e8371fd85547833a51cbec6ee2633
Mateusz Cegiełka <mateusz@cegla.net>
2019-08-06 15:08:39 +0200

    change issues link to /issues/new

--- evscode/src/internal/executor.rs
+++ evscode/src/internal/executor.rs
@@ -110,7 +110,7 @@ pub fn error_show(e: crate::E) {
 			Severity::Workflow => false,
 		};
 		let message =
-			format!("{}{}", e.human(), if should_suggest_report { "; [report issue?](https://github.com/pustaczek/icie/issues)" } else { "" });
+			format!("{}{}", e.human(), if should_suggest_report { "; [report issue?](https://github.com/pustaczek/icie/issues/new)" } else { "" });
 		let mut msg = crate::Message::new(message).error();
 		for (i, action) in e.actions.iter().enumerate() {
 			msg = msg.item(i.to_string(), action.title.as_str(), false);
--- src/template.rs
+++ src/template.rs
@@ -60,7 +60,7 @@ using namespace std;
 
 // 💖 Hi, thanks for using ICIE! 💖
 // 🔧 To use a custom code template, set it in Settings(Ctrl+,) in "Icie Template List" entry 🔧
-// 📝 If you spot any bugs or miss any features, create an issue at https://github.com/pustaczek/icie 📝
+// 📝 If you spot any bugs or miss any features, create an issue at https://github.com/pustaczek/icie/issues/new 📝
 
 int main() {
     ios::sync_with_stdio(false);
