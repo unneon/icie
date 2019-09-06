6a808c104e7ee55d929c1727c9574cf7d165757b
Mateusz Cegiełka <mateusz@cegla.net>
2019-09-06 22:54:25 +0200

    add util for deserializing json in uj

--- unijudge/src/json.rs
+++ unijudge/src/json.rs
@@ -1,9 +1,18 @@
 use crate::{Error, Result};
 use backtrace::Backtrace;
 use reqwest::Response;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Deserialize};
 
 pub fn from_resp<T: DeserializeOwned>(mut resp: Response, endpoint: &'static str) -> Result<T> {
 	let resp_raw = resp.text()?;
 	serde_json::from_str(&resp_raw).map_err(|e| Error::UnexpectedJSON { endpoint, backtrace: Backtrace::new(), resp_raw, inner: Some(Box::new(e)) })
+}
+
+pub fn from_str<'d, T: Deserialize<'d>>(resp_raw: &'d str, endpoint: &'static str) -> Result<T> {
+	serde_json::from_str(resp_raw).map_err(|e| Error::UnexpectedJSON {
+		endpoint,
+		backtrace: Backtrace::new(),
+		resp_raw: resp_raw.to_owned(),
+		inner: Some(Box::new(e)),
+	})
 }
