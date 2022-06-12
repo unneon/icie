ca10d8ee905d2664d15ed941e482776cd2168178
georgejsh <georgejsh@gmail.com>
2022-06-12 13:40:16 +0530

    test

--- src/submit.rs
+++ src/submit.rs
@@ -23,7 +23,7 @@ async fn send() -> R<()> {
 	send_after_tests_passed().await
 }
 
-#[evscode::command(title = "ICIE Get Rank List", key = "alt+7")]
+#[evscode::command(title = "ICIE Get Rank List", key = "alt+-")]
 pub async fn get_rank() -> R<()> {
 	let _status = crate::STATUS.push("Getting List");
 	let (sess, task) = connect_to_workspace_task().await?;
